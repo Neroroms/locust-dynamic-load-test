0053832634b29d525d61934246b3c4739ccc1cd5
Neroroms <wasu.chermsook@kingpower.com>
2020-04-14 06:21:02 +0000

    Change info 1. Add config 2. Change some variable 3. Move default file

--- load_test.py
+++ load_test.py
@@ -4,19 +4,37 @@ from support_function import *
 import csv, os, json
 
 scriptPath = os.path.abspath(os.path.dirname(__file__))
-loadTestCsvFileName = "loadTestInfo.csv"
+waitTimePerRequest = 2
+
+loadTestFilePath = ""
+loadTestFileInfoName = ""
+
+with open(path.join(scriptPath, "config.json")) as jsonFile:
+  configJson = json.load(jsonFile)
+
+  loadTestFilePath = path.join(scriptPath, configJson['loadTestFile']['path'])
+  loadTestFileInfoName = configJson['loadTestFile']['infoName'] + ".csv"
+
+  if path.exists(path.join(loadTestFilePath, loadTestFileInfoName)) == False:
+    print("Load test file not found.")
+    exit(1)
+
+  if len(configJson['waitTimePerRequest']) > 0:
+    waitTimePerRequest = int(configJson['waitTimePerRequest'])
+  else:
+    waitTimePerRequest = 2  
 
 #Setup load test information
 loadTestSetups = []
 
-with open(path.join(scriptPath, loadTestCsvFileName)) as loadTestCsv:
+with open(path.join(loadTestFilePath, loadTestFileInfoName)) as loadTestCsv:
   loadTestDatas = csv.reader(loadTestCsv)
 
   for loadTestData in loadTestDatas:
     testFileVariable = loadTestData[2]
 
     if len(testFileVariable) > 0:
-      with open(path.join(scriptPath, testFileVariable)) as jsonFile:
+      with open(path.join(loadTestFilePath,testFileVariable)) as jsonFile:
         pathVariables = json.load(jsonFile)
 
         for pathVariable in pathVariables['data']:
@@ -44,5 +62,5 @@ class loadTestTasks(TaskSet):
   tasks = locustTaskList
 
 class loadTest(HttpLocust):
-  wait_time = constant(2)
+  wait_time = constant(waitTimePerRequest)
   task_set = loadTestTasks
